2e9b05719536075d828b2a8e53783d5d55f6b8cb
Ismael <ismaelreyplata@gmail.com>
2025-12-29 17:38:07 +0100

    analizar cv

---
+++ Tema 2/cv_analyzer/services/cv_evaluator.py
@@ -0,0 +1,41 @@
+from langchain_google_genai import ChatGoogleGenerativeAI
+from models.cv_model import AnalisisCV
+from prompts.cv_prompts import crear_sistema_prompts
+
+def crear_evaluador_cv():
+    modelo_base = ChatGoogleGenerativeAI(
+        model="gemini-2.5-flash",
+        temperature=0.2
+    )
+
+    modelo_estructurado = modelo_base.with_structured_output(AnalisisCV)
+    chat_prompt = crear_sistema_prompts()
+    cadena_evaluacion = chat_prompt | modelo_estructurado
+
+    return cadena_evaluacion
+
+
+def evaluar_candidato(texto_cv: str, descripcion_puesto: str) -> AnalisisCV:
+    try:
+        cadena_evaluacion = crear_evaluador_cv()
+        resultado = cadena_evaluacion.invoke({
+            "texto_cv": texto_cv,
+            "descripcion_puesto": descripcion_puesto
+        })
+
+        return resultado
+    except Exception as e:
+        return AnalisisCV(
+            nombre_candidato="Error en procesamiento",
+            experiencia_años = 0,
+            habilidades_clave=["Error al procesar CV"],
+            education="No se puede determinar",
+            experiencia_relevante="Error durante el análisis",
+            fortalezas=["Requiere revision manual del CV"],
+            areas_mejora=["Verificar formato y legibilidad del PDF"],
+            porcentaje_ajuste=0,
+        )
+
+    
+
+    
